6fcd6799ec486e91263b4655f2e76f547b8fd6e4
P G <pgauchan@gmail.com>
2018-03-13 01:30:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  Object.assign({}, key);
+  Object.assign({}, key, value);
 }
