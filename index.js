33c851fef9c661c0239b20ec3eadb3de55656655
P G <pgauchan@gmail.com>
2018-03-13 01:16:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue(recipe, key, value){
+function updateObjectWithKeyAndValue(recipes, key, value){
   recipes[key] = value;
   return recipes;
 }
